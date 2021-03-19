415ff3fde9dac7eed54a2354941c23521b09503b
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2021-03-19 23:08:08 +0200

    ??

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
-import '../App.css';
 
 export default memo(function App() {
   const [good, setGood] = useState(0);
