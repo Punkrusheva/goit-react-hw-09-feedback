d0312a46900681e54ae594c5cdeb2ba2a5de9851
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2021-03-20 10:07:59 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import {
-  useEffect,
   useState,
   memo,
-  useRef,
 } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
@@ -15,8 +13,6 @@ export default memo(function App() {
   const [bad, setBad] = useState(0);
 
   const option = { good, neutral, bad };
-  const total = useRef(0);
-  const percent = useRef(0);
 
   const onLeaveFeedback = (option) => {
     switch (option) {
@@ -37,12 +33,6 @@ export default memo(function App() {
     }
   };
 
-  useEffect(() => {
-    total.current = good + neutral + bad;
-    percent.current = Math.round((100 * good) / total.current);
-    console.log(total.current, percent.current);
-  }, [good, neutral, bad]);
-
   return (
    <>
         <Section title="Pleas leave feedback">
@@ -51,50 +41,15 @@ export default memo(function App() {
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
-       {total.current > 0 && (<Section title="Statistics">
+       {good + neutral + bad > 0 && (<Section title="Statistics">
           <Statistics
             options={Object.keys(option)}
             state={option}
-            total={total.current}
-        positiveFeedback={percent.current > 0 ? percent.current : 0}
+            total={good + neutral + bad}
+        positiveFeedback={Math.round((100 * good) / (good + neutral + bad)) > 0 ? Math.round((100 * good) / (good + neutral + bad)) : 0}
         />
         </Section>)}
-        {total.current === 0 && (<Notification message="No feedback given"/>)}
+        {(good + neutral + bad) === 0 && (<Notification message="No feedback given"/>)}
       </>
 )
 });
-
-/*class OldApp extends Component {
-   
-  countTotalFeedback = () => { 
-    return Object.values(this.state).reduce((acc,value)=> acc+ value,0)
-  }
-
-  countPositiveFeedbackPercentage = () => {
-    const percent = Math.round((100 * this.state.good) / this.countTotalFeedback());
-    return percent>0 ? percent : 0;
-  }
-
-  render() {
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
-    
-    return (
-       <>
-        <Section title="Pleas leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}/>
-        </Section>
-        {total > 0 && (<Section title="Statistics">
-          <Statistics
-            options={Object.keys(this.state)}
-            state={this.state}
-            total={total}
-            positiveFeedback={positiveFeedback}/>
-        </Section>)}
-        {total === 0 && (<Notification message="No feedback given"/>)}
-      </>
-    )
-  }
-}*/
